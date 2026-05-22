584067d506cc27f782e2762d27a5fde84b319fab
joao-tec-info <joaoemanuelcarloslima2201@gmail.com>
2026-05-22 19:13:57 -0300

    estrutura montada para conecçao do mongo db

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,41 +1,15 @@
-from app.models.book_model import Book
-from app.models.student_model import Student
-from app.services.loan_service import LoanService
+from fastapi import FastAPI
+from app.config.database import db
 
 
-def main():
-
-    aluno = Student(
-    "João",
-    "2º Ano A"
-)
-
-    livro = Book(
-    "Dom Casmurro",
-    "123456",
-    10,
-    "Romance"
-)
-
-    emprestimo = LoanService.create_loan(
-        aluno,
-        livro,
-        "22/05/2026",
-        "29/05/2026"
-    )
-
-    if emprestimo:
-
-        print("Empréstimo realizado!")
-
-        print(emprestimo.aluno)
-
-        print(emprestimo.livro)
-
-    else:
-
-        print("Livro indisponível")
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def home():
+
+    db.command("ping")
+
+    return {
+        "message": "MongoDB Atlas conectado"
+    }
