3066ae1fd3e4a7479b14eaedc687201ba0388200
joao-tec-info <joaoemanuelcarloslima2201@gmail.com>
2026-05-22 23:18:50 -0300

    criaçao de rotas para os livros

--- backend/app/routes/books.py
+++ backend/app/routes/books.py
@@ -21,3 +21,44 @@ def create_book(book: BookCreate):
         "message": "Livro criado com sucesso",
         "id": str(result.inserted_id)
     }
+
+@router.get("/")
+def get_books():
+
+    books = list(db.books.find())
+
+    for b in books:
+        b["_id"] = str(b["_id"])
+
+    return books
+
+from bson import ObjectId
+
+
+@router.get("/{book_id}")
+def get_book(book_id: str):
+
+    book = db.books.find_one({"_id": ObjectId(book_id)})
+
+    if not book:
+        return {"error": "Livro não encontrado"}
+
+    book["_id"] = str(book["_id"])
+    return book
+
+@router.put("/{book_id}")
+def update_book(book_id: str, book: BookCreate):
+
+    db.books.update_one(
+        {"_id": ObjectId(book_id)},
+        {"$set": book.dict()}
+    )
+
+    return {"message": "Livro atualizado"}
+
+@router.delete("/{book_id}")
+def delete_book(book_id: str):
+
+    db.books.delete_one({"_id": ObjectId(book_id)})
+
+    return {"message": "Livro deletado"}
