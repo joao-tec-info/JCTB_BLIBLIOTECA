1e1bc5fdf5942c8552cae2b6db1e82d3db8ef480
joao-tec-info <joaoemanuelcarloslima2201@gmail.com>
2026-05-22 23:15:49 -0300

    rota livro (post )

--- backend/app/routes/books.py
+++ backend/app/routes/books.py
@@ -4,3 +4,20 @@ from app.schemas.book_schema import BookCreate
 from app.models.book_model import Book
 
 router = APIRouter(prefix="/books", tags=["Books"])
+
+@router.post("/")
+def create_book(book: BookCreate):
+
+    new_book = Book(
+        titulo=book.titulo,
+        sbm=book.sbm,
+        quantidade_total=book.quantidade_total,
+        categoria=book.categoria
+    )
+
+    result = db.books.insert_one(new_book.to_dict())
+
+    return {
+        "message": "Livro criado com sucesso",
+        "id": str(result.inserted_id)
+    }
